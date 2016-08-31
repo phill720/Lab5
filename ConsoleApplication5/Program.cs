d621c69b790af84dc2bfaa1242446dd0b0567762
phill720 <phillipschris902@gmail.com>
2016-08-31 13:42:06 -0400

    Dice operational

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
+            //------initial values------
+            int counter = 1;
+            bool con = true;
+            Console.WriteLine("\nWelcome to the Grand Circus Casino! Roll the dice?(y/n):");
+            //------program loop------
+            while (con)
+            {
+                Console.WriteLine("\nHow many sides for the dice?");
+                int sides = int.Parse(Console.ReadLine());
+                Console.WriteLine(RollBoth(sides, counter));
+                //------Continue?--------
+                Console.WriteLine("\nAnother one? (y/n)");
+                string ans = Console.ReadLine();
+                Console.WriteLine("\n================================");
+                if (ans == "y")
+                {
+                    con = true;
+                    counter++;
+                }
+                else
+                {
+                    con = false;
+                    Console.WriteLine("WE MADE IT!!!");
+                }
+            }
+        }
+        //-------Methods---------
+        static string RollBoth(int sides, int counter)
+        {
+            Random random = new Random();
+            int ranNum1 = random.Next(1, sides + 1);
+            int ranNum2 = random.Next(1, sides + 1);
+            string str = "\nRoll " + counter + ":\n" + ranNum1 + "\n" + ranNum2;
+            return str;
         }
     }
 }
