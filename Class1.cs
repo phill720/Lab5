6bc4ebcbf0ac53281dcb7075cd335ecde76d67ce
phill720 <phillipschris902@gmail.com>
2016-09-27 12:35:29 -0400

    including rolls class

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RngMethods
+{
+    public class Rolls
+    {
+        //-------Methods---------
+        public static string RollBoth(int sides, int counter)
+        {
+            Random random = new Random();
+            int ranNum1 = random.Next(1, sides + 1);
+            int ranNum2 = random.Next(1, sides + 1);
+            //----craps, box cars, snake eyes----
+            if (ranNum1 == 1 && ranNum2 == 1)
+            { Console.WriteLine("Snake Eyes!\nCraps!"); }
+            if (ranNum1 + ranNum2 == 3 || ranNum1 + ranNum2 == 12)
+            { Console.WriteLine("Craps!"); }
+            else if (ranNum1 == 6 && ranNum2 == 6)
+            { Console.WriteLine("BOX CARS!"); }
+            else if (ranNum1 + ranNum2 == 7 || ranNum1 + ranNum2 == 11)
+            { Console.WriteLine("Natural!"); }
+            //-------return string-------
+            string str = "Roll " + counter + ":\n" + ranNum1 + "\n" + ranNum2;
+            return str;
+        }
+    }
+}
